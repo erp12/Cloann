3db72efa1c08aa6aa496e73756254112b9c8ad01
erp12 <erp12@hampshire.edu>
2015-10-10 00:55:39 -0400

    Added activation functions

--- src/cloann/core.clj
+++ src/cloann/core.clj
@@ -3,22 +3,8 @@
   (:use clojure.core.matrix)
   (:use clojure.core.matrix.operators))
 
-(defn output->class
-  "Converts the output matrix of the ANN to a single number per observation,
-corrispoding to that observation's class"
-  [output-matrix]
-  (map (fn [v]
-         (.indexOf v 1))
-       output-matrix))
-
-(defn class->output
-  "Converts a vector of class numbers, into the desired ANN output matrix"
-  [class-num-vec]
-  (let [vector-len (apply max class-num-vec)]
-    ;(matrix
-      (vec
-        (map (fn [class-num]
-               (assoc (vec (take (inc vector-len) (repeat 0)))
-                      class-num
-                      1))
-             class-num-vec))))
+(defn feed-forward
+  [input-matrix weight-matrix bias-node-matrix]
+  (let [net
+        output]
+    ()))
---
+++ src/cloann/util.clj
@@ -0,0 +1,29 @@
+(ns cloann.util
+  (:use clojure.core.matrix)
+  (:use clojure.core.matrix.operators))
+
+(defn nat-log
+  "Implements LN function using Math/log"
+  [x]
+  (/ (Math/log x) (Math/log Math/E)))
+
+(defn output->class
+  "Converts the output matrix of the ANN to a single number per observation,
+corrispoding to that observation's class"
+  [output-matrix]
+  (map (fn [v]
+         (.indexOf v 1))
+       output-matrix))
+
+(defn class->output
+  "Converts a vector of class numbers, into the desired ANN output matrix"
+  [class-num-vec]
+  (let [vector-len (apply max class-num-vec)]
+    ;(matrix
+      (vec
+        (map (fn [class-num]
+               (assoc (vec (take (inc vector-len) (repeat 0)))
+                      class-num
+                      1))
+             class-num-vec))))
+
