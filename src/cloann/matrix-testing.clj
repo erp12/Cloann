763e9063957147bc6f3b704d45d7aace9c7bae78
Eddie <erp12@hampshire.edu>
2015-10-26 20:20:36 -0400

    Added XOR data

--- src/cloann/matrix-testing.clj
+++ src/cloann/matrix-testing.clj
@@ -1,8 +1,21 @@
 (ns cloann.matrix-testing
   (:gen-class)
+  (:use [cloann util activation-functions])
   (:use clojure.core.matrix)
   (:use clojure.core.matrix.operators))
 
 
-(def M1 (matrix [[1 2][3 4]]))
-(apply + (apply + M1))
+(def W (matrix [[0 1 2]
+                [3 4 5]
+                [6 7 8]]))
+
+(defn repeat-func-over-mat
+  [init-matrix func num]
+  (loop [m init-matrix
+         i 0]
+    (if (= i num)
+      m
+      (do
+        (println m)
+        (recur (emap func m)
+               (inc i))))))
