5a4b5100366aef26af859779d92268599ff5043c
erp12 <erp12@hampshire.edu>
2016-04-20 23:30:28 -0400

    improvments to error logs

--- gelnn_error_logs/merge_logs.py
+++ gelnn_error_logs/merge_logs.py
@@ -1,6 +1,6 @@
 import time, glob
 
-outfilename = 'all_' + str((int(time.time()))) + ".txt"
+outfilename = 'all_' + str((int(time.time()))) + "error_log.txt"
 filenames = glob.glob('*error_log.txt')
 
 with open(outfilename, 'w') as outfile:
